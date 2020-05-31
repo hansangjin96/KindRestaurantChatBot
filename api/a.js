56f62c2ce3bb9b7d88e122897e8bd852647f4076
hansangjin96 <s202188@naver.com>
2020-05-31 16:06:01 +0900

    한번더 입력할때 돌아감

--- api/a.js
+++ api/a.js
@@ -3,6 +3,8 @@
 const prompt = require("prompt-sync")();
 const AssistantV2 = require("ibm-watson/assistant/v2");
 const { IamAuthenticator } = require("ibm-watson/auth");
+var kindOfFood;
+var can = false;
 
 const service = new AssistantV2({
   version: "2020-05-28",
@@ -14,6 +16,33 @@ const service = new AssistantV2({
 
 const assistantId = "ee4a36a1-08a6-4afd-8d89-5bd5a00798a5";
 let sessionId;
+
+const parse = require("csv-parse");
+const fs = require("fs");
+var transform = require("stream-transform");
+var cityName;
+var kindOfFood;
+var parser = parse({ delimiter: "," });
+var cnt = 0;
+
+var input = fs.createReadStream("data.csv", { encoding: "utf-8" }); //파일명, 기본은 utf-8
+
+function fn_test(record) {
+  if (record[0] === kindOfFood) {
+    if (record[4] === cityName) {
+      console.log(
+        `이름 : ${record[1]} 대표매뉴 : ${record[7]}  가격 : ${record[8]}  주소 : ${record[5]}`
+      );
+    }
+  }
+}
+
+var transformer = transform(function (record, callback) {
+  if (cnt < 10 && cnt != 0) {
+    callback(fn_test(record));
+  }
+  cnt++;
+});
 
 service
   .createSession({
@@ -53,8 +82,16 @@ function processResponse(response) {
     console.log("Detected intent: #" + response.output.intents[0].intent);
   } else if (response.output.entities.length > 0) {
     console.log("Detected entity: @" + response.output.entities[0].entity);
-  }
 
+    if (response.output.entities[0].entity === "세부위치엔티티") {
+      cityName = response.output.entities[0].value;
+    }
+
+    if (response.output.entities[0].entity === "한식") {
+      kindOfFood = "한식";
+      input.pipe(parser).pipe(transformer).pipe(process.stdout);
+    }
+  }
   // Display the output from assistant, if any. Supports only a single
   // text response.
   if (response.output.generic) {
@@ -78,9 +115,11 @@ function processResponse(response) {
       });
     return;
   }
+
   newMessageInput = {
     messageType: "text",
     text: newMessageFromUser,
   };
+
   sendMessage(newMessageInput);
 }
