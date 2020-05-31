1c84a6c6e0db4548d0c46cf39f4b91a627604af9
hansangjin96 <s202188@naver.com>
2020-05-31 17:55:14 +0900

    d

--- app.js
+++ app.js
@@ -1,108 +1,77 @@
-/**
- *
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-'use strict';
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const colors = require('colors');
+var watson = require('./util/watson');
 
-var express = require('express'); // app server
-var bodyParser = require('body-parser'); // parser for post requests
-var AssistantV2 = require('ibm-watson/assistant/v2'); // watson sdk
-const { IamAuthenticator, BearerTokenAuthenticator } = require('ibm-watson/auth');
+//Route files
+const index = require('./routes/index');
+const app = express();
+dotenv.config({ path: './config/config.env' });
 
-var app = express();
-require('./health/health')(app);
 
-// Bootstrap application settings
-app.use(express.static('./public')); // load UI from public folder
-app.use(bodyParser.json());
+//Mount the router
+app.use('/api/v1', index);
 
-// Create the service wrapper
+app.use('/', express.static('public'));
 
-let authenticator;
-if (process.env.ASSISTANT_IAM_APIKEY) {
-  authenticator = new IamAuthenticator({
-    apikey: process.env.ASSISTANT_IAM_APIKEY
-  });
-} else if (process.env.BEARER_TOKEN) {
-  authenticator = new BearerTokenAuthenticator({
-    bearerToken: process.env.BEARER_TOKEN
-  });
-}
+const PORT = process.env.PORT;
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running on port ${PORT}`.yellow
+  )
+);
 
-var assistant = new AssistantV2({
-  version: '2019-02-28',
-  authenticator: authenticator,
-  url: process.env.ASSISTANT_URL,
-  disableSslVerification: process.env.DISABLE_SSL_VERIFICATION === 'true' ? true : false
+//Handel unhandled promise rejections
+process.on('unhandledRejection', (err, Promise) => {
+  console.log(`Error  ${err.message}`.red);
+  //close server & exit process
+  server.close(() => process.exit(1));
 });
 
-// Endpoint to be call from the client side
-app.post('/api/message', function(req, res) {
-  let assistantId = process.env.ASSISTANT_ID || '<assistant-id>';
-  if (!assistantId || assistantId === '<assistant-id>') {
-    return res.json({
-      output: {
-        text:
-          'The app has not been configured with a <b>ASSISTANT_ID</b> environment variable. Please refer to the ' +
-          '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' +
-          'Once a workspace has been defined the intents may be imported from ' +
-          '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.',
-      },
-    });
-  }
+var io = require('socket.io')(server);
+var ss = require('socket.io-stream');
+io.on('connection', function(socket) {
+  console.log('Connected');
 
-  var textIn = '';
+  var context = {};
+  socket.on('sendmsg', function(data) {
+    watson.message(data.message, context, function(err, res) {
+      if (!err) {
+        //console.log(res);
+        context = res.result.context;
+        console.log(res.result.output);
 
-  if (req.body.input) {
-    textIn = req.body.input.text;
-  }
-
-  var payload = {
-    assistantId: assistantId,
-    sessionId: req.body.session_id,
-    input: {
-      message_type: 'text',
-      text: textIn,
-    },
-  };
-
-  // Send the input to the assistant service
-  assistant.message(payload, function(err, data) {
-    if (err) {
-      const status = err.code !== undefined && err.code > 0 ? err.code : 500;
-      return res.status(status).json(err);
-    }
-
-    return res.json(data);
-  });
-});
-
-app.get('/api/session', function(req, res) {
-  assistant.createSession(
-    {
-      assistantId: process.env.ASSISTANT_ID || '{assistant_id}',
-    },
-    function(error, response) {
-      if (error) {
-        return res.send(error);
-      } else {
-        return res.send(response);
+        if (Array.isArray(res.result.output.text))
+          conversation_response = res.result.output.text[0];
+        //.join(' ').trim();
+        else conversation_response = undefined;
+        if (conversation_response) {
+          var payload = {
+            user: 'System',
+            message: conversation_response,
+            ts: new Date().getTime(),
+            type: data.type || 'text'
+          };
+          socket.emit('replymsg', payload);
+        }
       }
-    }
-  );
-});
+    });
+  });
 
-module.exports = app;
+  ss(socket).on('recognize', function(stream, data) {
+    watson.recognize(
+      stream,
+      function(err) {
+        console.log('Error:', err);
+      },
+      function(res) {
+        var transcript = res;
+        socket.emit('transcript', { message: transcript, ts: data.ts });
+        console.log(JSON.stringify(res, null, 2));
+      }
+    );
+  });
+});
