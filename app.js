63f8bd5e67696b029024a3db7ba041b482eceb62
YangSH-hub <61305117+YangSH-hub@users.noreply.github.com>
2020-06-02 16:14:20 +0900

    Revert "지역, 업종에 따라 가게 추천 기능 추가"
    This reverts commit 0ffb476e49928fcbefacfb7f2b2b83e58959a30c.

--- app.js
+++ app.js
@@ -1,147 +1,75 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const morgan = require("morgan");
-const colors = require("colors");
-const parse = require("csv-parse/lib/sync");
-const fs = require("fs");
-const csv = fs.readFileSync("data.csv");
-const records = parse(csv.toString("utf-8"));
 
-const findFoodEntity = "업종";
-const findCityEntity = "세부위치엔티티";
-
-var kindOfFood = "한식";
-var cityName = "강남구";
-
-var watson = require("./util/watson");
-
-var csvData = [];
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const colors = require('colors');
+var watson = require('./util/watson');
 
 //Route files
-const index = require("./routes/index");
+const index = require('./routes/index');
 const app = express();
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
+
 
 //Mount the router
-app.use("/api/v1", index);
+app.use('/api/v1', index);
 
-app.use("/", express.static("public"));
+app.use('/', express.static('public'));
 
 const PORT = process.env.PORT;
 const server = app.listen(
   PORT,
-  console.log(`Server running on port ${PORT}`.yellow)
+  console.log(
+    `Server running on port ${PORT}`.yellow
+  )
 );
 
 //Handel unhandled promise rejections
-process.on("unhandledRejection", (err, Promise) => {
+process.on('unhandledRejection', (err, Promise) => {
   console.log(`Error  ${err.message}`.red);
   //close server & exit process
   server.close(() => process.exit(1));
 });
 
-var io = require("socket.io")(server);
-var ss = require("socket.io-stream");
-
-io.on("connection", function (socket) {
-  console.log("Connected");
+var io = require('socket.io')(server);
+var ss = require('socket.io-stream');
+io.on('connection', function(socket) {
+  console.log('Connected');
 
   var context = {};
-  socket.on("sendmsg", function (data) {
-    watson.message(data.message, context, function (err, res) {
+  socket.on('sendmsg', function(data) {
+    watson.message(data.message, context, function(err, res) {
       if (!err) {
         //console.log(res);
         context = res.result.context;
-        console.log(res.result);
+        console.log(res.result.output);
 
-        if (Array.isArray(res.result.output.text)) {
-          if (res.result.entities.length > 0) {
-            if (res.result.entities[0].entity === findFoodEntity) {
-              kindOfFood = res.result.entities[0].value;
-              for (let i = 0; i < records.length; i++) {
-                if (records[i][0] === kindOfFood && records[i][4] === cityName)
-                  csvData.push(records[i][1]);
-              }
-              conversation_response = csvData;
-            } else if (res.result.entities[0].entity === findCityEntity) {
-              cityName = res.result.entities[0].value;
-              conversation_response = res.result.output.text[0];
-            } else {
-              conversation_response = res.result.output.text[0];
-            }
-          } else {
-            conversation_response = res.result.output.text[0];
-          }
-
-          //conversation_response = res.result.output.text[0];
-          /*option 처리
-          if (
-            res.result.output.generic[res.result.output.generic.length - 1]
-              .response_type.length > 0
-          ) {
-            conversation_response +=
-              res.result.output.generic[res.result.output.generic.length - 1]
-                .title;
-            for (let i = 0; i < res.result.output.generic.length - 1; i++) {
-              conversation_response +=
-                result.output.generic[res.result.output.generic.length - i]
-                  .text;
-            }
-          }
-          */
-          //text 길이에 따라 출력
-          /*
-          if (res.result.output.text.length === 1) {
-            if (res.result.entities.value === "한식") {
-              console.log("한식!!!!!!!!!!!!!!!!!!!");
-
-              callReadFile(function (data) {
-                resultData = data;
-                console.log(resultData);
-              });
-            }
-            conversation_response = res.result.output.text[0];
-          } else if (res.result.output.text.length > 1) {
-            
-            conversation_response = res.result.entities.value;
-
-            if (res.result.entities.value === "한식") {
-              console.log("한식!!!!!!!!!!!!!!!!!!!");
-
-              callReadFile(function (data) {
-                resultData = data;
-                console.log(resultData);
-              });
-            }
-            
-            conversation_response =
-            res.result.output.text[res.result.output.text.length - 1];
-          }*/
-        }
+        if (Array.isArray(res.result.output.text))
+          conversation_response = res.result.output.text[0];
         //.join(' ').trim();
         else conversation_response = undefined;
         if (conversation_response) {
           var payload = {
-            user: "System",
+            user: 'System',
             message: conversation_response,
             ts: new Date().getTime(),
-            type: data.type || "text",
+            type: data.type || 'text'
           };
-          socket.emit("replymsg", payload);
+          socket.emit('replymsg', payload);
         }
       }
     });
   });
 
-  ss(socket).on("recognize", function (stream, data) {
+  ss(socket).on('recognize', function(stream, data) {
     watson.recognize(
       stream,
-      function (err) {
-        console.log("Error:", err);
+      function(err) {
+        console.log('Error:', err);
       },
-      function (res) {
+      function(res) {
         var transcript = res;
-        socket.emit("transcript", { message: transcript, ts: data.ts });
+        socket.emit('transcript', { message: transcript, ts: data.ts });
         console.log(JSON.stringify(res, null, 2));
       }
     );
